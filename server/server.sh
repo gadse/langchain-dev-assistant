0cc26e70c306b48d013b96deebcc040ee3638c84
Peter Bozsoky <peter-bozsoky@posteo.de>
2024-03-19 16:33:30 +0100

    server script: Add explanatory comment

--- server/server.sh
+++ server/server.sh
@@ -1,4 +1,5 @@
 export OLLAMA_HOST="127.0.0.1:8001"
+# Without this, ollama will keep telling you to pull the model because it can´t find its models
 export OLLAMA_MODELS=/usr/share/ollama/.ollama/models
 
 ollama serve
