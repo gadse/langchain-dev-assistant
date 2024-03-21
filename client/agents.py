a9cd803b4958d2d1dd3e275c936c8c207bd23f28
Peter Bozsoky <peter-bozsoky@posteo.de>
2024-03-21 09:13:51 +0100

    agent: Move and make proper
    Moving the agent code all to its proper place.
    
    Also, add several tools to the agent and add a better initial
    prompt so it knows:
     - its purpose
     - its tools
     - and what its tools are capable of

--- client/agents.py
+++ client/agents.py
@@ -1,12 +1,31 @@
+"""
+See Also:
+    https://python.langchain.com/docs/modules/agents/
+"""
+
 import requests
 
+from langchain import LLMMathChain
+from langchain import hub
+from langchain.agents import AgentType, initialize_agent, create_react_agent, AgentExecutor
+from langchain.tools import (
+    BaseTool,
+    StructuredTool,
+    Tool,
+    tool,
+    DuckDuckGoSearchRun,
+    StackExchangeTool,
+)
 from langchain.llms.base import LLM
+from langchain_community.utilities import StackExchangeAPIWrapper
+from langchain_experimental.utilities import PythonREPL
 
 from typing import Optional, List, Mapping, Any
 
 
-HOST = 'localhost:5000'
-URI = f'http://{HOST}/api/v1/generate'
+HOST = "localhost:5000"
+URI = f"http://{HOST}/api/v1/generate"
+
 
 class AlpacaLLM(LLM):
     @property
@@ -25,25 +44,25 @@ class AlpacaLLM(LLM):
                 "max_new_tokens": 256,
                 "early_stopping": True,
                 "stopping_strings": stop,
-                'do_sample': True,
-                'top_p': 0.1,
-                'typical_p': 1,
-                'repetition_penalty': 1.18,
-                'top_k': 40,
-                'min_length': 0,
-                'no_repeat_ngram_size': 0,
-                'num_beams': 1,
-                'penalty_alpha': 0,
-                'length_penalty': 1,
-                'seed': -1,
-                'add_bos_token': True,
-                'truncation_length': 2048,
-                'ban_eos_token': False,
-                'skip_special_tokens': True,
+                "do_sample": True,
+                "top_p": 0.1,
+                "typical_p": 1,
+                "repetition_penalty": 1.18,
+                "top_k": 40,
+                "min_length": 0,
+                "no_repeat_ngram_size": 0,
+                "num_beams": 1,
+                "penalty_alpha": 0,
+                "length_penalty": 1,
+                "seed": -1,
+                "add_bos_token": True,
+                "truncation_length": 2048,
+                "ban_eos_token": False,
+                "skip_special_tokens": True,
             },
         )
         response.raise_for_status()
-        return response.json()['results'][0]['text']
+        return response.json()["results"][0]["text"]
 
     @property
     def _identifying_params(self) -> Mapping[str, Any]:
@@ -52,3 +71,84 @@ class AlpacaLLM(LLM):
 
 
 llm = AlpacaLLM()
+
+
+def build_tooled_agent(llm):
+    """
+    Used https://www.comet.com/site/blog/enhancing-langchain-agents-with-custom-tools/
+    """
+    stack_exchange = StackExchangeAPIWrapper()
+    stack_exchange_tool = Tool.from_function(
+        func=stack_exchange.run,
+        name="StackExchange",
+        description="You coding and programming related search engine for StackExchange. Useful for when you need to search"
+                    "the internet for coding, programming, or computer related information.",
+    )
+
+    # duck_duck_go = DuckDuckGoSearchRun()
+    # search_tool = Tool.from_function(
+    #     func=duck_duck_go.run,
+    #     name="Internet Search",
+    #     description="Your internet search engine. Useful for when you need to search the internet for general"
+    #                 "information on people, places, history, etc.",
+    # )
+
+    llm_math_chain = LLMMathChain(llm=llm, verbose=True)
+    math_tool = Tool.from_function(
+        func=llm_math_chain.run,
+        name="Calculator",
+        description="Your handy calculator. Useful for when you are asked to perform math calculations",
+    )
+
+    python_repl = PythonREPL()
+    repl_tool = Tool(
+        name="Python REPL",
+        description="A Python shell. Use this to execute python commands. Input should be a valid python"
+                    "command. If you want to see the output of a value, you should print it out with"
+                    "`print(...)`.",
+        func=python_repl.run,
+    )
+
+    instructions = """
+    You are an agent designed to help a developer write code, reason about code,
+    and to look up information about programming concepts.
+    """
+
+    instructions += """
+    You can use your StackExchange tool to search specifically for coding, programming, or computer
+    related information.
+    """
+
+    # instructions += """
+    # You can also use your internet search tool to search for general information.
+    # """
+
+    instructions += """
+    You can also use your handy calculator tool for performing math calculations.
+    """
+
+    instructions += """
+    You can also use your Python REPL too to write and execute python code to answer questions.
+    You have access to a python REPL, which you can use to execute python code.
+    If you get an error, debug your code and try again.
+    You can use the output of your code to answer the question.
+    You might know the answer without running any code, but you should still run the code to get the answer.
+    If it does not seem like you can write code to answer the question, just return "I don't know" as the answer.
+    """
+    # base_prompt = hub.pull("langchain-ai/react-agent-template")
+    base_prompt = hub.pull("hwchase17/react")
+    prompt = base_prompt.partial(instructions=instructions)
+
+    tools = [
+        stack_exchange_tool,
+        #search_tool,
+        math_tool,
+        repl_tool
+    ]
+    agent = create_react_agent(
+        llm,
+        tools,
+        prompt
+    )
+
+    return AgentExecutor(agent=agent, tools=tools, verbose=True)
--- client/main.py
+++ client/main.py
@@ -8,20 +8,15 @@ import os
 
 import dotenv
 
-from agents import AlpacaLLM
-
 from langchain import PromptTemplate
-from langchain import hub
-from langchain.agents import Tool
-from langchain.agents import AgentExecutor
 from langchain.chains import LLMChain
 from langchain.chains import SimpleSequentialChain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.chat_models import ChatOllama
 from langchain_community.llms import Ollama
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_experimental.utilities import PythonREPL
+
+from client.agents import build_tooled_agent
 
 
 def greet():
@@ -48,26 +43,20 @@ def build_llm():
 def build_chain(llm):
     prompt = PromptTemplate(
         input_variables=["llm_persona"],
-        template="You are {llm_persona}. Please explain the concept of a large language model."
+        template="You are {llm_persona}. Please explain the concept of a large language model.",
     )
 
-    expert_chain = LLMChain(
-        llm=llm,
-        prompt=prompt
-    )
+    expert_chain = LLMChain(llm=llm, prompt=prompt)
 
     eli5_chain = LLMChain(
         llm=llm,
         prompt=PromptTemplate(
             input_variables=["llm_persona"],
-            template="You are {llm_persona}. Now please explain this like I am five years old in 500 words."
-        )
+            template="You are {llm_persona}. Now please explain this like I am five years old in 500 words.",
+        ),
     )
 
-    return SimpleSequentialChain(
-        chains=[expert_chain, eli5_chain],
-        verbose=True
-    )
+    return SimpleSequentialChain(chains=[expert_chain, eli5_chain], verbose=True)
 
 
 def ask_prompt(sequential_chain):
@@ -87,17 +76,16 @@ def ask_prompt(sequential_chain):
 
 
 def split(text):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=100,
-        chunk_overlap=0
-    )
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=0)
     return text_splitter.create_documents([text])
 
 
 def store(splitted):
     embeddings_model = os.environ.get("HUGGINGFACE_EMBEDDING_MODEL")
-    model_kwargs = {'device': 'cuda'}
-    embeddings = HuggingFaceEmbeddings(model_name=embeddings_model, model_kwargs=model_kwargs)
+    model_kwargs = {"device": "cuda"}
+    embeddings = HuggingFaceEmbeddings(
+        model_name=embeddings_model, model_kwargs=model_kwargs
+    )
 
     vector_store = FAISS.from_documents(splitted, embeddings)
 
@@ -111,81 +99,17 @@ def search(vector_store):
     return vector_store.similarity_search(query)
 
 
-def build_tooled_agent(llm):
-    """
-    Used https://www.comet.com/site/blog/enhancing-langchain-agents-with-custom-tools/
-    """
-    from langchain import LLMMathChain
-    from langchain.agents import AgentType, initialize_agent
-    from langchain.chat_models import ChatOpenAI
-    from langchain.tools import BaseTool, StructuredTool, Tool, tool, DuckDuckGoSearchRun, StackExchangeTool
-    from langchain_community.utilities import StackExchangeAPIWrapper
-
-    # duck_duck_go = DuckDuckGoSearchRun()
-    # search_tool = Tool.from_function(
-    #     func=duck_duck_go.run,
-    #     name="Search",
-    #     description="useful for when you need to search the internet for information"
-    # )
-
-    # llm_math_chain = LLMMathChain(llm=llm, verbose=True)
-    # math_tool = Tool.from_function(
-    #     func=llm_math_chain.run,
-    #     name="Calculator",
-    #     description="Useful for when you are asked to perform math calculations"
-    # )
-
-    stack_exchange = StackExchangeAPIWrapper()
-    stack_exchange_tool = Tool.from_function(
-        func=stack_exchange.run,
-        name="Stack Exchange",
-        description="Useful for when you neet to search the internet for programming or computer related information"
-    )
-
-    tools = [stack_exchange_tool]
-    agent = initialize_agent(
-        tools,
-        llm,
-        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-        verbose=True
-    )
-
-    return agent
-
-
-# def create_agent():
-#     python_repl = PythonREPL()
-#     repl_tool = Tool(
-#         name="python_repl",
-#         description="A Python shell. Use this to execute python commands. Input should be a valid python command. If you want to see the output of a value, you should print it out with `print(...)`.",
-#         func=python_repl.run
-#     )
-#     agent_tools = [repl_tool]
-#
-#     instructions = """You are an agent designed to write and execute python code to answer questions.
-#     You have access to a python REPL, which you can use to execute python code.
-#     If you get an error, debug your code and try again.
-#     Only use the output of your code to answer the question.
-#     You might know the answer without running any code, but you should still run the code to get the answer.
-#     If it does not seem like you can write code to answer the question, just return "I don't know" as the answer.
-#     """
-#     base_prompt = hub.pull("langchain-ai/react-agent-template")
-#     prompt = base_prompt.partial(instructions=instructions)
-#     agent = create_(ChatOllama(temperature=0), agent_tools, prompt)
-#     return AgentExecutor(agent=agent, tools=agent_tools, verbose=True)
-
-
 if __name__ == "__main__":
     greet()
     pre_flight_checks()
     start = time.process_time()
 
     llm = build_llm()
-    chain = build_chain(llm)
-    response = ask_prompt(chain)
-    splitted = split(response)
-    vector_store = store(splitted)
-    print(search(vector_store))
+    # chain = build_chain(llm)
+    # response = ask_prompt(chain)
+    # splitted = split(response)
+    # vector_store = store(splitted)
+    # print(search(vector_store))
 
     agent = build_tooled_agent(llm)
     agent.invoke({"input": "What is the 10th fibonacci number?"})
